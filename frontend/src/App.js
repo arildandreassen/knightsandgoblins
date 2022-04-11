0e5de8fbc153c88f20e7baf4322e4256887b3e93
Arild Andreassen <arildsa@hotmail.com>
2022-04-10 18:20:34 -0600

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>knights and goblins</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
